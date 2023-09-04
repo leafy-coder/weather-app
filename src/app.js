e7b24cf36257424df2cac9053809b6c4b8fa121c
Arunava Ghosh <54508035+drifted-coder@users.noreply.github.com>
2023-09-04 20:30:11 +0530

    Created the folder structure

--- src/app.js
+++ src/app.js
@@ -1,13 +1,48 @@
+// Importing express
 const express = require('express');
-
 const app = express();
 
+//----------------------------------
+
+// imporing path variable 
+const path = require('path');
+
+//----------------------------------
+
+// Intializing path for both server and local
 const port = process.env.PORT || 8081;
 
-app.get('/',(req,res)=>{
-    res.send("Express Working");
+//----------------------------------
+
+// Setting static files to use
+const staticPath = path.join(__dirname,'..','public');
+app.use(express.static(staticPath))
+
+//----------------------------------
+
+// Setting view engine and views files to use
+const templatePath = path.join(__dirname,'..','views');
+app.set('view engine','hbs');
+app.set('views',templatePath)
+
+//----------------------------------
+
+
+// All Page Routes
+// ---------------------------------
+app.get('/index',(req,res)=>{
+    res.render('index')
 })
 
+
+
+
+
+
+
+
+// Application startup port setup
+//----------------------------------
 app.listen(port, ()=>{
     console.log(`Application is running successfully on ${port}`);
 })
